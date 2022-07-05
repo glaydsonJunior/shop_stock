24e985f75b396e868494ae95b98e401dca4ea180
glaydson.junior <102086284+glaydsonJunior@users.noreply.github.com>
2022-07-05 10:02:15 -0300

    Update basic_setup.py

--- shop_system/basic_setup.py
+++ shop_system/basic_setup.py
@@ -6,5 +6,5 @@ host = input('Host: ')
 passwd = input('Password: ')
 port = input('Port: ')
 tabela = Crud_psql(user,host,passwd,database,port)
-
+tabela.criar_tabela()
 print("Tabela criada com sucesso!")
